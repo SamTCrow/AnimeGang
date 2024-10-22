a8551e48b54ae78d7b6bc2eea14739308dd868be
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2024-10-22 20:03:32 +0200

    App skeleton

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@vueuse/nuxt"],
+  compatibilityDate: "2024-10-22",
+});
